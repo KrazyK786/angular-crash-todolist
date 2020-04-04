13c577541c4cac792688932e2aaa310237badf3f
Kristopher Johnson <krazyk786@gmail.com>
2020-04-04 17:13:39 -0500

    generated to-do component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ToDosComponent } from './components/to-dos/to-dos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDosComponent
   ],
   imports: [
     BrowserModule
