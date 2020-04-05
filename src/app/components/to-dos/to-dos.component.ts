e29b3dff67a0adb9064bcd2f8ffb81c1e951a501
Kristopher Johnson <krazyk786@gmail.com>
2020-04-04 19:24:02 -0500

    created Todo model and implemented toDos array into to-do component

--- src/app/components/to-dos/to-dos.component.ts
+++ src/app/components/to-dos/to-dos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-to-dos',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDosComponent implements OnInit {
 
+  toDos: Todo[];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.toDos = [
+      {
+        id: 1,
+        title: 'Todo 1',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Todo 2',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Todo 3',
+        completed: false
+      }
+    ];
   }
 
 }
